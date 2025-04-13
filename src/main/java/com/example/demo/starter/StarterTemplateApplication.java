7b47f598f58f64a2cdbd2ee5591df473476581cf
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-04-14 00:44:58 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/starter/StarterTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/starter/StarterTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

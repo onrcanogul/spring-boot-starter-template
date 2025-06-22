4fe85bea92c9c56187f23381604977b512201d2b
Onurcan Oğul <147406204+onrcanogul@users.noreply.github.com>
2025-06-22 09:08:47 +0300

    minor changes

--- src/main/java/com/example/demo/starter/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/demo/starter/repository/RefreshTokenRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
     Optional<RefreshToken> findByToken(String token);
 }
